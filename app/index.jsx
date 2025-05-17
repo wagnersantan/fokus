a0719b39664be37ebef35135c0d5447eafd89fe5
Wagner do Amparo Santana <38575562+wagnersantan@users.noreply.github.com>
2025-05-17 10:15:47 -0300

    Adicionei suporte Android e ajustes no projeto

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -9,7 +9,37 @@ export default function Index() {
       />
       
       <View style={styles.actions}>
+        <View style={styles.context}>
+          <Pressable style={styles.contextButtonActive}>
+            <Text style={styles.contextButtonText}>
+              Foco
+            </Text>
+          </Pressable>
+          <Pressable>
+            <Text style={styles.contextButtonText}>
+              Pausa Curta
+            </Text>
+          </Pressable>
+          <Pressable>
+            <Text style={styles.contextButtonText}>
+              Pausa Longa
+            </Text>
+          </Pressable>
+        </View>
         <Text style={styles.timer}>25:00</Text>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>
+            Começar
+          </Text>
+        </Pressable>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>
+          Projeto fictício e sem fins comerciais. 
+        </Text>
+        <Text style={styles.footerText}>
+          Desenvolvido por Wagner Santana
+        </Text>
       </View>
     </View>
   );
@@ -31,7 +61,23 @@ const styles = StyleSheet.create({
     borderRadius: 32,
     borderWidth: 2,
     borderColor: '#444880',
+    alignItems: 'center',
+    gap: 32
+  },
+  context: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
     alignItems: 'center'
+   
+  },
+  contextButtonActive: {
+    backgroundColor: '#144480',
+    borderRadius: 8
+  },
+    contextButtonText: {
+    fontSize: 12.5,
+    color: '#FFF',
+    padding: 8
   },
   pomodoro: {
     width: 180,
@@ -39,7 +85,29 @@ const styles = StyleSheet.create({
     resizeMode: "contain"
   },
   timer: {
-    fontSize: 36,
-    color: '#FFF'
+    fontSize: 54,
+    color: '#FFF',
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  button: {
+    backgroundColor: '#8872FF',
+    borderRadius: 32,
+    padding: 8,
+    width: '100%',
+    alignItems: 'center'
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#211223',
+    fontSize: 18
+  },
+  footer: {
+    width: '80%'
+  },
+  footerText: {
+    textAlign: 'center',
+    color: '#98A0A8',
+    fontSize: 15.5
   }
 });
